e31b869ebf3b2487a6309bb370d1b051b411a54a
AmarjithTK <amarjithraveendran@gmail.com>
2021-12-17 17:24:09 +0530

    firebase_options-sucks7

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import '../screens/registration_screen.dart';
 import '../screens/chat_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_option.dart';
 import 'firebase_options.dart';
 
 void main() async {
